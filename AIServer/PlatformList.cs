0da632b4e11ad8456ba94d739a4d62fef3dec54c
Administrator <Administrator@58Y5T3A7WQ9K14Q>
2020-07-16 23:53:09 +0800

    推广平台底层

--- AIServer/PlatformList.cs
+++ AIServer/PlatformList.cs
@@ -1,9 +1,12 @@
 ﻿using AIDB.Models;
+using AIServer.Reqs;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using YL.Base;
+using YL.Base.Manager.Entity;
 
 namespace AIServer
 {
@@ -13,9 +16,111 @@ namespace AIServer
         {
         }
 
-        public List<Platforminfo> ss() 
+        public List<Platforminfo> ss()
         {
             return db.Platforminfo.Where(w => w.Id > 0).ToList();
+        }
+
+        /// <summary>
+        /// 根据条件查询  推广平台列表
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        public AjaxResult<Pagination<Platforminfo>> GetList(PlatforminfoReq req)
+        {
+            Pagination<Platforminfo> page = new Pagination<Platforminfo>();
+            var query = from b in db.Platforminfo
+                        select b;
+            if (!string.IsNullOrWhiteSpace(req.PlatformName))
+            {
+                query = query.Where(w => w.PlatformName.Contains(req.PlatformName));
+            }
+            if (!string.IsNullOrWhiteSpace(req.AddressURL))
+            {
+                query = query.Where(w => w.AddressUrl.Contains(req.AddressURL));
+            }
+            page.TotalCount = query.Count();
+            page.dataList = query.OrderByDescending(m => m.Id).Skip((req.PageIndex - 1) * req.PageSize).Take(req.PageSize).ToList();
+            return new AjaxResult<Pagination<Platforminfo>>(page);
+        }
+
+        /// <summary>
+        /// 添加推广平台
+        /// </summary>
+        /// <param name="req"></param>
+        /// <returns></returns>
+        public AjaxResult<Object> AddPlatforminfo(PlatforminfoReq req)
+        {
+            Platforminfo model_1 = db.Platforminfo.Where(w => w.PlatformName == req.PlatformName || w.AddressUrl == req.AddressURL).FirstOrDefault();
+            if (model_1 != null)
+            {
+                return new AjaxResult<Object>("推广平台已存在！");
+            }
+
+            Platforminfo model = new Platforminfo();
+            model.PlatformName = req.PlatformName;
+            model.AddressUrl = req.AddressURL;
+            model.CreateTime = DateTime.Now;
+            model.Remark = req.Remark;
+            db.Platforminfo.Add(model);
+            if (db.SaveChanges() > 0)
+            {
+                return new AjaxResult<Object>("添加成功！", 0);
+            }
+            return new AjaxResult<Object>("添加失败！");
+        }
+
+        /// <summary>
+        /// 编辑推广平台
+        /// </summary>
+        /// <param name="req"></param>
+        /// <returns></returns>
+        public AjaxResult<Object> EditPlatforminfo(PlatforminfoReq req)
+        {
+            Platforminfo model = db.Platforminfo.Where(w => w.Id == req.ID).FirstOrDefault();
+            if (model == null)
+            {
+                return new AjaxResult<Object>("推广平台不存在！");
+            }
+            model.PlatformName = req.PlatformName;
+            model.AddressUrl = req.AddressURL;
+            model.Remark = req.Remark;
+            if (db.SaveChanges() > 0)
+            {
+                return new AjaxResult<Object>("保存成功！", 0);
+            }
+            return new AjaxResult<Object>("保存失败！");
+
+        }
+
+        /// <summary>
+        /// 删除推广平台
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public AjaxResult<Object> DelPlatforminfo(long id)
+        {
+            Platforminfo model = db.Platforminfo.Where(w => w.Id == id).FirstOrDefault();
+            if (model == null)
+            {
+                return new AjaxResult<Object>("推广平台不存在！");
+            }
+            db.Platforminfo.Remove(model);
+            if (db.SaveChanges() > 0)
+            {
+                return new AjaxResult<Object>("删除成功！", 0);
+            }
+            return new AjaxResult<Object>("删除失败！");
+        }
+
+        /// <summary>
+        /// 查询推广平台详细
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public Platforminfo SelPlatforminfo(long id)
+        {
+            return db.Platforminfo.Where(w => w.Id == id).FirstOrDefault();
         }
     }
 }
---
+++ AIServer/Reqs/PlatforminfoReq.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using YL.Base.Manager.Entity;
+
+namespace AIServer.Reqs
+{
+    /// <summary>
+    /// 推广平台
+    /// </summary>
+    public class PlatforminfoReq: PageModel
+    {
+        /// <summary>
+        /// ID
+        /// </summary>
+        public long ID { get; set; }
+        /// <summary>
+        /// 平台名称
+        /// </summary>
+        public string PlatformName { get; set; }
+        /// <summary>
+        /// 平台网址
+        /// </summary>
+        public string AddressURL { get; set; }
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime CreateTime { get; set; }
+        /// <summary>
+        /// 备注信息
+        /// </summary>
+        public string Remark { get; set; }
+    }
+}
