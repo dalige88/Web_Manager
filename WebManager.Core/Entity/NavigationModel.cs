f350cc68b9cdfbedcdc42e33284cc0e28a5de362
yangli-1128 <yangli-1128@163.com>
2020-07-20 23:03:57 +0800

    [fix] move NavigationModel

--- WebManager.Core/Entity/NavigationModel.cs
+++ WebManager.Core/Entity/NavigationModel.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Web.Manager.WebManager.Entity
+namespace WebManager.Core.Entity
 {
     public class NavigationModel
     {
